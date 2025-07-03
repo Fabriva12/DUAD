2f761e11f6758f760385c15a93aff816acfc7d58
fabricio vargas alfaro <fabricio.12@live.com>
2025-07-03 11:56:42 -0600

    correccion semana 11

--- Semana11/circle.py
+++ Semana11/circle.py
@@ -1,10 +1,10 @@
 class Circle:
-    radius = 4
+    def __init__(self, radius):
+        self.radius = radius 
     def get_area(self):
         area = 3.14 * self.radius ** 2
         print (f"{area}")
-my_circle = Circle()
-my_circle.radius = 6
-my_circle_2 = Circle()
+my_circle = Circle(6)
+my_circle_2 = Circle(4)
 my_circle.get_area()
 my_circle_2.get_area()
