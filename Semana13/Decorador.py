a8d579696c3895b40f2ae79beea51d10cdd5126e
fabricio vargas alfaro <fabricio.12@live.com>
2025-07-08 10:06:18 -0600

    correcciones primer ejercicio

--- Semana13/Decorador.py
+++ Semana13/Decorador.py
@@ -1,13 +1,13 @@
 def print_parameters(func):
     def wrapper (*args):
         print ("Parameter", args)
-        func(*args)
+        result = func(*args)
+        print (f"The triangle area is {result}")
     return wrapper
 
 @print_parameters
 def get_triangle_area(base, height):
     triangle_area = (base * height) / 2
-    print(f"The triangle area is {triangle_area}")
-
+    return triangle_area
 
 get_triangle_area(4,6)
