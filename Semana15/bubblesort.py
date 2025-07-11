bda218e4134f969f39622c35c24ee6275796a1b1
fabricio vargas alfaro <fabricio.12@live.com>
2025-07-11 13:37:39 -0600

    se agrego swapped a las funciones

--- Semana15/bubblesort.py
+++ Semana15/bubblesort.py
@@ -1,5 +1,6 @@
 def bubble_sort(list_to_sort):
     for outer_index in range(0, len(list_to_sort)-1):
+        swapped = False
         for index in range(0, len(list_to_sort)-1-outer_index):
             current_element = list_to_sort[index]
             next_element = list_to_sort[index +1]
@@ -8,6 +9,9 @@ def bubble_sort(list_to_sort):
                 print('El elemento actual es mayor al siguiente. Intercambiandolos...')
                 list_to_sort[index] = next_element
                 list_to_sort[index+1] = current_element
+                swapped = True
+        if not swapped:
+            break
     print(f"{list_to_sort}")
 my_list = [23,-23,55,32,11,-77,44,10]
 bubble_sort(my_list)
--- Semana15/bubblesort2.py
+++ Semana15/bubblesort2.py
@@ -1,11 +1,15 @@
 def bubble_sort_invert(my_list):
     for outer_index in range (len(my_list)-1):
+        swapped = False
         for index in range (len(my_list)-1,0,-1):
             current_element = my_list[index]
             next_element = my_list[index -1]
             if next_element > current_element:
                 my_list[index] = next_element
                 my_list[index -1] = current_element
+                swapped = True
+        if not swapped:
+            break
     print(f"{my_list}")
 
 my_list = [22,23,44,55,77,88,99,21,33]
